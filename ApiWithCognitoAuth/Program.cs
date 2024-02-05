295ee4813559e6198c5a4e808a2fd3c4233ec28a
El Mehdi Smiri <ElMehdi.Smiri@One-Beyond.com>
2024-02-05 19:30:46 +0100

    changes

--- ApiWithCognitoAuth/Program.cs
+++ ApiWithCognitoAuth/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddCognitoIdentity();
+//builder.Services.AddCognitoIdentity();
 builder.Services
     .AddAuthentication(options =>
     {
